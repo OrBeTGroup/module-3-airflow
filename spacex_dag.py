5076dc8d233fe0e555c0f6a84cdc15981161e157
OrBeTGroup <48784181+OrBeTGroup@users.noreply.github.com>
2021-03-03 15:59:09 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -15,7 +15,7 @@ default_args = {
 }
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
-for i in ["falcon1", "falcon9", "falconheavy"]
+for i in ("falcon1", "falcon9", "falconheavy")
     t1 = BashOperator(
         task_id="get_data", 
         bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r " + i, 
