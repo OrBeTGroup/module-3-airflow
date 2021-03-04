d7743377d05b8a9b32a988fc713d6ef0cf0ba2fa
OrBeTGroup <48784181+OrBeTGroup@users.noreply.github.com>
2021-03-04 14:06:13 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2018, 1, 1),
+    "start_date": datetime(2005, 1, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -18,15 +18,14 @@ dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
 for name_rocket in ["all", "falcon1", "falcon9", "falconheavy"]:
     t1 = BashOperator(
-        task_id="get_data_"+name_rocket,
-        #bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{ params.rocket }}" if name_rocket !='all' else ""),
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data{}".format(" -r {{ params.rocket }}" if name_rocket !='all' else ""),
+        task_id="get_data_" + name_rocket,
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{ params.rocket }}" if name_rocket !='all' else ""),
         params={"rocket": name_rocket},
         dag=dag
     )
 
     t2 = BashOperator(
-        task_id="print_data_"+name_rocket, 
+        task_id="print_data_" + name_rocket, 
         bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
         params={"rocket": name_rocket}, # falcon1/falcon9/falconheavy
         dag=dag
