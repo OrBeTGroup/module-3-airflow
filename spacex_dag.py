06df127038e60abad4513978aed3a60ffd8faed7
OrBeTGroup <48784181+OrBeTGroup@users.noreply.github.com>
2021-03-04 13:31:21 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -16,39 +16,19 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-for i in ["all", "falcon1", "falcon9", "falconheavy"]:
+for name_rocket in ["all", "falcon1", "falcon9", "falconheavy"]:
     t1 = BashOperator(
-        task_id="get_data_"+i,
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{ params.rocket }}" if i !='all' else ""),
-        #bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data",
-        #bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r "+ i, 
-        params={"rocket": i},
+        task_id="get_data_"+name_rocket,
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{ params.rocket }}" if name_rocket !='all' else ""),
+        #params={"rocket": name_rocket},
         dag=dag
     )
 
     t2 = BashOperator(
-        task_id="print_data_"+i, 
+        task_id="print_data_"+name_rocket, 
         bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-        #params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-        params={"rocket": i}, # falcon1/falcon9/falconheavy
-        #bash_command="cat /var/data/year={{ execution_date.year }}/rocket="+i+"/data.csv", 
+        params={"rocket": name_rocket}, # falcon1/falcon9/falconheavy
         dag=dag
     )
 
     t1 >> t2
-"""
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
-
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
-
-t1 >> t2
-"""
